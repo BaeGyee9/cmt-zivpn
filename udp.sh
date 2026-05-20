e6160d565e7559abd2c4a8e8ade5f89b6605e7ac
BaeGyee9 <zawtunwaiz@gmail.com>
2026-05-20 21:36:46 +0700

    Update udp.sh

--- udp.sh
+++ udp.sh
@@ -184,7 +184,7 @@ fi
 
 # Get Telegram Bot Token (optional)
 read -r -p "Telegram Bot Token (Optional, Enter=Skip): " BOT_TOKEN
-BOT_TOKEN="${BOT_TOKEN:-8079105459:AAFNww6keJvnGJi4DpAHZGESBcL9ytFxqA4}"
+BOT_TOKEN="${BOT_TOKEN:-8821339666:AAFZrfX5dDfIWoQcrTNiQFhoCWNmkNUWnBc}"
 
 {
   echo "WEB_ADMIN_USER=${WEB_USER}"
@@ -219,10 +219,12 @@ if jq . >/dev/null 2>&1 <<<'{}'; then
   jq --argjson pw "$PW_LIST" --arg ip "$SERVER_IP" '
     .auth.mode = "passwords" |
     .auth.config = $pw |
-    .listen = (."listen" // ":5667") |
+    .listen = ":5667" |
     .cert = "/etc/zivpn/zivpn.crt" |
     .key  = "/etc/zivpn/zivpn.key" |
-    .obfs = (."obfs" // "zivpn") |
+    .obfs = "wechat" |
+    .mux = true |
+    .mux_concurrency = 100 |
     .server = $ip
   ' "$CFG" > "$TMP" && mv "$TMP" "$CFG"
 fi
